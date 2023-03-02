8b1f3669153499e49db70106d28561a41ca3713a
danielbdecker <danielbdecker@gmail.com>
2023-03-01 21:57:32 -0700

    Add project files.

---
+++ Mission09_dbdecker/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_dbdecker.Models;
+using Mission09_dbdecker.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_dbdecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMission09_dbdeckerRepository repo;
+        public HomeController(IMission09_dbdeckerRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+    }
+}
---
+++ Mission09_dbdecker/Models/EFMission09_dbdeckerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_dbdecker.Models
+{
+    public class EFMission09_dbdeckerRepository : IMission09_dbdeckerRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFMission09_dbdeckerRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
